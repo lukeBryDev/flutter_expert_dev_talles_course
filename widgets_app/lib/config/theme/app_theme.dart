785552c97386fb9f0787bb64d18ccca8578dc1be
Brayan <developergbp44@gmail.com>
2024-04-19 11:41:06 -0500

    widgets_app style: home screen created body: basic

--- widgets_app/lib/config/theme/app_theme.dart
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -26,5 +26,6 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
         colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
       );
 }
