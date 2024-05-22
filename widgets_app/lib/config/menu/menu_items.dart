e43921bf1dc9a805f7522c91590d250c651eb1c9
brayanbasca <developergbp44@gmail.com>
2024-05-22 14:05:55 -0500

    widgets_app feat(infinite_scroll_screen): screen finished body: final seccion 10

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -49,4 +49,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Pequeño tutorial introductorio',
       link: '/tutorial',
       icon: Icons.accessibility_rounded),
+  MenuItem(
+      title: 'Infinite scroll y Pull',
+      subTitle: 'Listas infinitas y pull to refresh',
+      link: '/infinite',
+      icon: Icons.list_alt_rounded),
 ];
