bc53b7e0f2fd29870c9fa06698e575f2488acef2
brayanbasca <developergbp44@gmail.com>
2024-05-23 16:06:42 -0500

    widgets_app feat(theme_changer_screen): theme integration with riverpod body: basic form

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+    final int selectedColor = ref.watch(selectedIndexColorProvider);
     return MaterialApp.router(
       title: 'Flutter widgets',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 5).getTheme(),
+      theme: AppTheme(selectedColor: selectedColor, isDarkMode: isDarkMode)
+          .getTheme(),
     );
   }
 }
--- widgets_app/lib/presentation/providers/theme_provider.dart
+++ widgets_app/lib/presentation/providers/theme_provider.dart
@@ -1,3 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
+// Listado de colores inmutable
+final colorListProvider = Provider((ref) => colorList);
+
+// Un simple boolean
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
+
+// Un simple int
+final selectedIndexColorProvider = StateProvider<int>((ref) => 0);
