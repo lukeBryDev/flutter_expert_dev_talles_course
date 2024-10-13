0238009455b1ee200ab2483e551affa7bf00c44f
brayanbasca <developergbp44@gmail.com>
2024-10-13 16:24:12 -0500

    push_app feat: creation of project body: section 21

---
+++ push_app/lib/presentation/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Permisos'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                // TODO: Solicitar permiso de notificaciones
+              },
+              icon: const Icon(Icons.settings)),
+        ],
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 0,
+        itemBuilder: (context, index) {
+      return const ListTile();
+    });
+  }
+}
